e0169492b9ceffb23911587021d0ba0247ed87c3
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-07-28 23:36:04 +0700

    Add project files.

---
+++ PermissionExtensions/PermissionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using OpenMod.API.Plugins;
+using OpenMod.Unturned.Plugins;
+using System;
+
+[assembly: PluginMetadata("PermissionExtensions", Author = "DiFFoZ", DisplayName = "Permission Extensions")]
+
+namespace PermissionExtensions
+{
+    public class PermissionExtensions : OpenModUnturnedPlugin
+    {
+        public PermissionExtensions(IServiceProvider serviceProvider) : base(serviceProvider)
+        {
+        }
+    }
+}
