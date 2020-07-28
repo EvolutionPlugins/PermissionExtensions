823024e28a0c717b6060d5c84ee7f5e97ff0a3b9
DiFFoZ <48765566+DiFFoZ@users.noreply.github.com>
2020-07-28 23:45:11 +0700

    Add PermissionRoleExtension

---
+++ PermissionExtensions/Models/PermissionRoleExtension.cs
@@ -0,0 +1,51 @@
+﻿using OpenMod.API.Permissions;
+using OpenMod.Core.Permissions;
+using OpenMod.Core.Permissions.Data;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+
+namespace PermissionExtensions.Models
+{
+    public class PermissionRoleExtension : IPermissionRole
+    {
+        public PermissionRoleExtension()
+        {
+            Parents = new HashSet<string>(StringComparer.InvariantCultureIgnoreCase);
+        }
+
+        public PermissionRoleExtension(PermissionRoleData data)
+        {
+            Id = data.Id;
+            Priority = data.Priority;
+            DisplayName = data.DisplayName;
+            Parents = data.Parents;
+            IsAutoAssigned = data.IsAutoAssigned;
+            Permissions = data.Permissions;
+        }
+
+        public static implicit operator PermissionRole(PermissionRoleExtension role)
+        {
+            return new PermissionRole(new PermissionRoleData()
+            {
+                Id = role.Id,
+                Priority = role.Priority,
+                DisplayName = role.DisplayName,
+                IsAutoAssigned = role.IsAutoAssigned,
+                Parents = role.Parents,
+                Permissions = role.Permissions
+            });
+        }
+
+        public string Id { get; }
+        public int Priority { get; set; }
+        public string DisplayName { get; }
+        public HashSet<string> Parents { get; }
+        public HashSet<string> Permissions { get; }
+        public bool IsAutoAssigned { get; set; }
+        public string Type { get; } = "role";
+        public string Prefix { get; set; }
+        public string Suffix { get; set; }
+        public Color Color { get; set; }
+    }
+}
